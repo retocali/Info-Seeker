f640b81bb78534eb6e385cc42f9f4075e4ac05db
Rodrigo <retocali@mit.edu>
2017-06-20 17:03:23 -0400

    Made all tiles be random

--- js/mainGame.js
+++ js/mainGame.js
@@ -2,16 +2,15 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload:
 var width = 3;
 var length = 3;
 var tileSize = 140;
-var tileNames = ["Corner_tile.png","Cross_Tile.png","DeadEnd_Tile.png","Line_Tile.png","Tetris_Tile.png"];
+var tileNames = ["Corner_Tile.png","Cross_Tile.png","DeadEnd_Tile.png","Line_Tile.png","Tetris_Tile.png"];
+var tiles = [];
 function preload() {
 
-    //  You can fill the preloader with as many assets as your game requires
-
-    //  Here we are loading an image. The first parameter is the unique
-    //  string by which we'll identify the image later in our code.
-
-    //  The second parameter is the URL of the image (relative)
-    game.load.image('tile', 'assets/sprites/tiles/' + tileNames[Math.floor(Math.random()*6)]);
+    
+    for (var i = 0; i < tileNames.length; i++) {
+        game.load.image('tile'+i, 'assets/sprites/tiles/' + tileNames[i]);
+        tiles.push('tile'+i);
+    }
 
 }
 
@@ -22,7 +21,7 @@ function create() {
     //  displays it on-screen
     for (var x = 0; x < width; x++) {
         for (var y = 0; y < length; y++) {
-            let s = game.add.sprite(game.world.centerX+x*tileSize-width/2*tileSize, game.world.centerY+y*tileSize-length/2*tileSize, 'tile');
+            let s = game.add.sprite(game.world.centerX+x*tileSize-width/2*tileSize, game.world.centerY+y*tileSize-length/2*tileSize, tiles[Math.floor(Math.random()*tiles.length)]);
             s.anchor.setTo(0.5,0.5);
             s.inputEnabled = true;
             s.events.onInputDown.add(function () { s.angle += 90;}, s)
